d7d0d428db4cb6b1546c6bcbdaeea8865e329e4c
Alegraleeson <wingmonkee@yahoo.com>
2022-06-25 12:40:05 -0500

    ascii art

--- password.js
+++ password.js
@@ -1,5 +1,5 @@
 let password = "ihatepasswords1"
-let passCheck = password.split('')
+let passCheck = password.split('') //I didn't use this variable, but left it in since it was mentioned to use
 let hasLetter = false
 let hasNumber = false
 let hasSpace = false
@@ -16,8 +16,6 @@ for(let i=0; i<password.length; i++){
  } 
 }
 
-// checks if password has a space and changes variable hasSpace to true if it does"
-
 for(let i=0; i<password.length; i++){
     if (password.charAt(i) === " ") {
      hasSpace = true
@@ -26,46 +24,36 @@ for(let i=0; i<password.length; i++){
 
 
 //checks if variable password has at least 10 characters but not more than 20, and if hasLetter, hasNumber, & hasSpace are true or false.   I checked whether to switch them to true earlier.
+//console logs ASCII art
 
 if (password.length >= 10 && password.length <= 20 && hasLetter === true && hasNumber === true && hasSpace === false) {
-    console.log('Success')
+    console.log(`
+    /$$$$$$                                                            
+   /$$__  $$                                                           
+  | $$  \__/ /$$   /$$  /$$$$$$$  /$$$$$$$  /$$$$$$   /$$$$$$$ /$$$$$$$
+  |  $$$$$$ | $$  | $$ /$$_____/ /$$_____/ /$$__  $$ /$$_____//$$_____/
+   \____  $$| $$  | $$| $$      | $$      | $$$$$$$$|  $$$$$$|  $$$$$$ 
+   /$$  \ $$| $$  | $$| $$      | $$      | $$_____/ \____  $$\____  $$
+  |  $$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$$ /$$$$$$$//$$$$$$$/
+   \______/  \______/  \_______/ \_______/ \_______/|_______/|_______/ 
+                                                                       
+                                                                       
+                                                                       
+  `)
 } else {
-    console.log('Failure!')
+    console.log(`
+    ____  __    ____  __    __  __  ____  ____ 
+   ( ___)/__\  (_  _)(  )  (  )(  )(  _ \( ___)
+    )__)/(__)\  _)(_  )(__  )(__)(  )   / )__) 
+   (__)(__)(__)(____)(____)(______)(_)\_)(____)
+   `)
 }
 
 //extras
-//checked if password has space and is less than 20 characters
+//made sure password has no spaces and is less than 20 characters and used ASCII art
 
-
-// String Length
-
-// Need help with how to calculate the length of your password? This article should help.
-
-// Additional Checks
-// There are many other constraints you might want to check for the password.
-
-// Add more constraints and checks to your program. You can read more about
-
-// String methods in Javascript here.
-
-// If you want to loop over your string you have to convert it into an array using the split method.Find more information here
-
-// Add at least 2 other checks and write a comment in your code listing them.
-
-// Here are some ideas, feel free to use these or come up with your own.
 
 // Check for presence of uppercase, lowercase, special characters, etc.
 
-// Check for maximum length (i.e. no more than 20 characters)
-
-// Make sure there aren’t spaces
-
 // Check for forbidden words (“password”, “letmein”)
-
-// Going Even Further
-// Add some ASCII Art to the output of your program!
-
-// In order to use ASCII art in your output, you’ll need to use create a multi-line string. More info here .
-
-// Be sure to push your code to Github for this assignment!
 
